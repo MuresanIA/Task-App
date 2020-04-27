d4a20f1c674174dce4bd72c639c0701f2640a2e5
MuresanIA <muresan.alexandru933@gmail.com>
2020-04-27 16:00:47 +0300

    Added 2 .sql folders in META-INF (have to go back on them and modify the code to work)

---
+++ src/main/resources/META-INF/queryTask.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM todo_app.task
+-- Date: 2020-04-26 10:01
+*/
+INSERT INTO `task` (`task_id`,`created_at`,`description`,`in_progress`,`project_project_id`,`user_id`) VALUES (1,'2020-04-26 09:55:07.365000','A simple to do','0',NULL,1);
+INSERT INTO `task` (`id`,`created_at`,`description`,`in_progress`,`project_project_id`,`user_id`) VALUES (2,'2020-04-26 09:57:03.197000','another 1','1',NULL,1);
+INSERT INTO `task` (`id`,`created_at`,`description`,`in_progress`,`project_project_id`,`user_id`) VALUES (3,'2020-04-26 09:57:06.413000','...','0',NULL,1);
+INSERT INTO `task` (`id`,`created_at`,`description`,`in_progress`,`project_project_id`,`user_id`) VALUES (4,'2020-04-26 09:57:09.014000','potatoes','0',NULL,1);
